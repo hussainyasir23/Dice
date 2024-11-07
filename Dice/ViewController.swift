15c0b77534f094589e7ec5adca163109bd2f60e5
Yasir Hussain <hussainyasir23@gmail.com>
2024-11-07 11:13:53 +0530

    Made some code improvements and animated the dice roll.

--- Dice/ViewController.swift
+++ Dice/ViewController.swift
@@ -8,110 +8,156 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        self.view = MyHomeView(frame: UIScreen.main.bounds)
+        self.view = HomeView(frame: UIScreen.main.bounds)
     }
-
 }
 
-class MyHomeView: UIView {
+class HomeView: UIView {
+    
+    // MARK: - UI Elements
+    
+    private let background: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "GreenBackground.png")
+        imageView.isUserInteractionEnabled = true
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    private let dice1: UIImageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    private let dice2: UIImageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    private let stView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .horizontal
+        stackView.distribution = .fillEqually
+        stackView.alignment = .center
+        stackView.spacing = 30
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
+    }()
+    
+    private let rollButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Roll", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.titleLabel?.font = .systemFont(ofSize: 25, weight: .bold)
+        button.backgroundColor = UIColor(red: 0.6, green: 0.1, blue: 0.1, alpha: 1)
+        button.layer.cornerRadius = 10
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    private var diceArray: [UIImage] {
+        return [
+            UIImage(named: "DiceOne")!,
+            UIImage(named: "DiceTwo")!,
+            UIImage(named: "DiceThree")!,
+            UIImage(named: "DiceFour")!,
+            UIImage(named: "DiceFive")!,
+            UIImage(named: "DiceSix")!
+        ]
+    }
+    
+    // MARK: - Initialization
     
     override init(frame: CGRect) {
-        
         super.init(frame: frame)
         setupScreen()
-        setupConstraints()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
-    func setupScreen(){
-        
-        self.addSubview(background)
+    // MARK: - Setup
+    
+    private func setupScreen() {
+        setupBackground()
+        setupDiceStackView()
+        setupRollButton()
+    }
+    
+    private func setupBackground() {
+        addSubview(background)
+        NSLayoutConstraint.activate([
+            background.leftAnchor.constraint(equalTo: self.leftAnchor),
+            background.rightAnchor.constraint(equalTo: self.rightAnchor),
+            background.topAnchor.constraint(equalTo: self.topAnchor),
+            background.bottomAnchor.constraint(equalTo: self.bottomAnchor)
+        ])
+    }
+    
+    private func setupDiceStackView() {
         background.addSubview(stView)
         stView.addArrangedSubview(dice1)
         stView.addArrangedSubview(dice2)
-        background.addSubview(stView)
+        
+        updateDiceImages()
+        
+        NSLayoutConstraint.activate([
+            stView.centerXAnchor.constraint(equalTo: centerXAnchor),
+            stView.centerYAnchor.constraint(equalTo: centerYAnchor, constant: -50),
+            stView.widthAnchor.constraint(equalToConstant: 230),
+            stView.heightAnchor.constraint(equalToConstant: 100)
+        ])
+    }
+    
+    private func setupRollButton() {
         background.addSubview(rollButton)
+        rollButton.addTarget(self, action: #selector(onClickRoll), for: .touchUpInside)
+        
+        NSLayoutConstraint.activate([
+            rollButton.centerXAnchor.constraint(equalTo: centerXAnchor),
+            rollButton.topAnchor.constraint(equalTo: stView.bottomAnchor, constant: 30),
+            rollButton.widthAnchor.constraint(equalToConstant: 100),
+            rollButton.heightAnchor.constraint(equalToConstant: 50)
+        ])
     }
     
-    func setupConstraints(){
-        
-        setBackgroundConstraints()
-        setDice()
-        setRollButton()
-    }
-    
-    func setBackgroundConstraints(){
-        
-        background.image = UIImage(named: "GreenBackground.png")
-        background.isUserInteractionEnabled = true
-        background.translatesAutoresizingMaskIntoConstraints = false
-        background.leftAnchor.constraint(equalTo: self.leftAnchor, constant: 0).isActive = true
-        background.topAnchor.constraint(equalTo: self.topAnchor, constant: 0).isActive = true
-        background.rightAnchor.constraint(equalTo: self.rightAnchor, constant: 0).isActive = true
-        background.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: 0).isActive = true
-    }
-    
-    func setDice(){
-        
-        stView.translatesAutoresizingMaskIntoConstraints = false
-        stView.leftAnchor.constraint(equalTo: centerXAnchor, constant: -115).isActive = true
-        stView.topAnchor.constraint(equalTo: centerYAnchor, constant: -150).isActive = true
-        stView.rightAnchor.constraint(equalTo: centerXAnchor, constant: 115).isActive = true
-        stView.bottomAnchor.constraint(equalTo: centerYAnchor, constant: -50).isActive = true
-        
-        stView.distribution = .fillEqually
-        stView.alignment = .center
-        stView.spacing = 30
-        
-        dice1.image = diceArray.randomElement()
-        dice2.image = diceArray.randomElement()
-    }
-    
-    func setRollButton(){
-        
-        rollButton.addTarget(self, action: #selector(self.onClickRoll), for: .touchUpInside)
-        rollButton.setTitle("Roll", for: .normal)
-        rollButton.layer.cornerRadius = 10
-        rollButton.titleLabel?.font = .systemFont(ofSize: 25)
-        rollButton.backgroundColor = #colorLiteral(red: 0.6075378656, green: 0.1114451364, blue: 0.1189513579, alpha: 1)
-        rollButton.isUserInteractionEnabled = true
-        
-        rollButton.translatesAutoresizingMaskIntoConstraints = false
-        rollButton.leftAnchor.constraint(equalTo: centerXAnchor, constant: -50).isActive = true
-        rollButton.topAnchor.constraint(equalTo: centerYAnchor, constant: 50).isActive = true
-        rollButton.rightAnchor.constraint(equalTo: centerXAnchor, constant: 50).isActive = true
-        rollButton.bottomAnchor.constraint(equalTo: centerYAnchor, constant: 100).isActive = true
-    }
-    
-    @objc func onClickRoll(){
+    // MARK: - Actions
+    @objc private func onClickRoll() {
         animateRollButton(rollButton)
+        animateDiceRoll()
+        updateDiceImages()
+    }
+    
+    // MARK: - Helper
+    
+    private func updateDiceImages() {
         dice1.image = diceArray.randomElement()
         dice2.image = diceArray.randomElement()
     }
     
-    func animateRollButton(_ viewToAnimate:UIButton){
-        UIView.animate(withDuration: 0.15, delay: 0, usingSpringWithDamping: 0.2, initialSpringVelocity: 0.5, options: .curveEaseIn, animations: {
-            
-            viewToAnimate.transform = CGAffineTransform(scaleX: 0.92, y: 0.92)
-        }) { (_) in
-            UIView.animate(withDuration: 0.15, delay: 0, usingSpringWithDamping: 0.4, initialSpringVelocity: 2, options: .curveEaseIn, animations: {
-                
-                viewToAnimate.transform = CGAffineTransform(scaleX: 1, y: 1)
-            }, completion: nil)
+    private func animateRollButton(_ button: UIButton) {
+        UIView.animate(withDuration: 0.15, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.5, options: .curveEaseInOut, animations: {
+            button.transform = CGAffineTransform(scaleX: 0.9, y: 0.9)
+        }) { _ in
+            UIView.animate(withDuration: 0.15, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.5, options: .curveEaseInOut, animations: {
+                button.transform = CGAffineTransform.identity
+            })
         }
     }
     
-    let background = UIImageView()
-    let dice1 = UIImageView()
-    let dice2 = UIImageView()
-    let stView = UIStackView()
-    let rollButton = UIButton()
-    let diceArray = [#imageLiteral(resourceName: "DiceOne"),#imageLiteral(resourceName: "DiceTwo"),#imageLiteral(resourceName: "DiceThree"),#imageLiteral(resourceName: "DiceFour"),#imageLiteral(resourceName: "DiceFive"),#imageLiteral(resourceName: "DiceSix")]
+    private func animateDiceRoll() {
+        UIView.animate(withDuration: 0.3, animations: {
+            self.dice1.transform = CGAffineTransform(rotationAngle: .pi)
+            self.dice2.transform = CGAffineTransform(rotationAngle: .pi)
+        }) { _ in
+            self.dice1.transform = .identity
+            self.dice2.transform = .identity
+        }
+    }
 }
